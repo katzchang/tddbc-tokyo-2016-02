0c96b78d81860b3d08da4f5958f5ced210e5a3e3
katzchang <katzchang@gmail.com>
2016-02-27 17:35:47 +0900

    refactor

--- src/test/scala/hello/GridPointTest.scala
+++ src/test/scala/hello/GridPointTest.scala
@@ -3,24 +3,21 @@ package hello
 import org.scalatest.FreeSpec
 
 class GridPointTest extends FreeSpec {
-  "grid point" in {
+  "GridPoint" - {
     val target = GridPoint(4, 7)
     assert(target.x == 4)
     assert(target.y == 7)
     assert(target.notation == "(4,7)")
-  }
 
-  "grid point hasSameCoordinatesWith" in {
-    val target = GridPoint(4, 7)
-    assert(target.hasSameCoordinatesWith(GridPoint(4, 7)))
-    assert(!target.hasSameCoordinatesWith(GridPoint(3, 8)))
-  }
+    "hasSameCoordinatesWith" in {
+      assert(target.hasSameCoordinatesWith(GridPoint(4, 7)))
+      assert(!target.hasSameCoordinatesWith(GridPoint(3, 8)))
+    }
 
-  "isNeighborOf" in {
-    val target = GridPoint(4, 7)
-    assert(target.isNeighborOf(GridPoint(3, 7)))
-    assert(!target.isNeighborOf(GridPoint(3, 8)))
-    assert(!target.isNeighborOf(GridPoint(4, 7)))
+    "isNeighborOf" in {
+      assert(target.isNeighborOf(GridPoint(3, 7)))
+      assert(!target.isNeighborOf(GridPoint(3, 8)))
+      assert(!target.isNeighborOf(GridPoint(4, 7)))
+    }
   }
-
 }
