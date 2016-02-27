8dd153977d3990e2a6fe9007e9b3eb2e347ce525
katzchang <katzchang@gmail.com>
2016-02-27 17:52:10 +0900

    ...

--- src/main/scala/hello/GridPoint.scala
+++ src/main/scala/hello/GridPoint.scala
@@ -6,14 +6,11 @@ case class GridPoint(p: (Int, Int)) {
 
   def hasSameCoordinatesWith(that: GridPoint) = this == that
 
-  def isNeighborOf(that: GridPoint) = {
-    val p2 = that.p
-    p2 == (x - 1, y) ||
-      p2 == (x + 1, y) ||
-      p2 == (x, y - 1) ||
-      p2 == (x, y + 1)
-
-  }
+  def isNeighborOf(that: (Int, Int)) =
+    that ==(x - 1, y) ||
+      that ==(x + 1, y) ||
+      that ==(x, y - 1) ||
+      that ==(x, y + 1)
 }
 
 object GridPoint {
