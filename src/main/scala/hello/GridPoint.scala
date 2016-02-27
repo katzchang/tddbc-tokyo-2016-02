3d8a7f0f399f63e128113784dd63297ef77bdb41
katzchang <katzchang@gmail.com>
2016-02-27 13:41:43 +0900

    hasSameCoordinatesWith

--- src/main/scala/hello/GridPoint.scala
+++ src/main/scala/hello/GridPoint.scala
@@ -2,4 +2,5 @@ package hello
 
 case class GridPoint(x: Int, y: Int) {
   val notation = (x, y).toString
+  def hasSameCoordinatesWith(that: GridPoint) = this == that
 }
--- src/test/scala/hello/GridPointTest.scala
+++ src/test/scala/hello/GridPointTest.scala
@@ -9,4 +9,11 @@ class GridPointTest extends FreeSpec {
     assert(target.y == 7)
     assert(target.notation == "(4,7)")
   }
+
+  "grid point hasSameCoordinatesWith" in {
+    val target = GridPoint(4, 7)
+    assert(target.hasSameCoordinatesWith(GridPoint(4, 7)) == true)
+    assert(target.hasSameCoordinatesWith(GridPoint(3, 8)) == false)
+  }
+
 }
