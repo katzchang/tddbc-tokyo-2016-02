e6cd0a806948a864cfef68a58855feec31289bb1
katzchang <katzchang@gmail.com>
2016-02-27 16:22:46 +0900

    ...

--- src/main/scala/hello/GridPoints.scala
+++ src/main/scala/hello/GridPoints.scala
@@ -1,12 +1,10 @@
 package hello
 
 case class GridPoints(private val points: GridPoint*) {
-  def isConnected = {
-    points.forall {
-      p =>
-        points.exists(p.isNeighborOf)
+  def isConnected =
+    points.forall { p =>
+      points.exists(p.isNeighborOf)
     }
-  }
 
   def contains(point: GridPoint) = points.contains(point)
 }
