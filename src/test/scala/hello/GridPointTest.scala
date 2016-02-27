07beb62d77ad40e1c1ca0ad9a52ec8049995ae2d
katzchang <katzchang@gmail.com>
2016-02-27 13:32:15 +0900

    add test

---
+++ src/test/scala/hello/GridPointTest.scala
@@ -0,0 +1,12 @@
+package hello
+
+import org.scalatest.FreeSpec
+
+class GridPointTest extends FreeSpec {
+  "grid point" in {
+    val target = GridPoint(4, 7)
+    assert(target.x == 4)
+    assert(target.y == 7)
+    assert(target.notation == "(4,7)")
+  }
+}
