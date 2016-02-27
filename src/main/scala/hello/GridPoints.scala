b80212ea1b8e671cbe2ab9ff6655fb830f4e5987
katzchang <katzchang@gmail.com>
2016-02-27 14:33:01 +0900

    connected

--- src/main/scala/hello/GridPoints.scala
+++ src/main/scala/hello/GridPoints.scala
@@ -1,6 +1,8 @@
 package hello
 
 case class GridPoints(a: GridPoint, b: GridPoint) {
+  def isConnected = a.isNeighborOf(b)
   def contains(that: GridPoint) =
     that == a || that == b
+
 }
--- src/test/scala/hello/GridPointsTest.scala
+++ src/test/scala/hello/GridPointsTest.scala
@@ -8,4 +8,9 @@ class GridPointsTest extends FreeSpec {
     assert(target.contains(GridPoint(4,7)))
     assert(!target.contains(GridPoint(4,8)))
   }
+
+  "isConnected" in {
+    assert(GridPoints(GridPoint(4, 7), GridPoint(3, 7)).isConnected)
+    assert(!GridPoints(GridPoint(4, 7), GridPoint(3, 8)).isConnected)
+  }
 }
